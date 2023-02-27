dd70dd688d9f7f77b605e9d8c3950eeae26fdee5
o-wiszowaty <ppwiszu@gmail.com>
2023-02-27 17:31:27 +0100

    Init commit

---
+++ src/main/java/com/couchpoker/couchpoker/CouchPokerApplication.java
@@ -0,0 +1,13 @@
+package com.couchpoker.couchpoker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouchPokerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CouchPokerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/couchpoker/couchpoker/CouchPokerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.couchpoker.couchpoker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouchPokerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
